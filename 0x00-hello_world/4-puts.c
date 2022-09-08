a8aef7152bf7b928c09e9b33a29a9aa6bfcba189
DukeProspero <sotinoyeprosper01@gmail.com>
2022-09-08 14:28:42 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,21 +4,21 @@
 
 /**
  *
- *  * main - Entry point
+ *  * main - runs the all the code
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    * Description: Run puts and print statement
  *
- *     */
+ *     * Return: Return 0 to exit function
+ *
+ *      */
 
 int main(void)
 
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
-
-
 
 			return (0);
 
