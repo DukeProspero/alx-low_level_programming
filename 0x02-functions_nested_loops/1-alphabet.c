17cf3e42ec1fe14541231bc21f6a315bbde3ae67
DukeProspero <sotinoyeprosper01@gmail.com>
2022-09-13 14:45:18 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,18 +12,4 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
 }
 _putchar('\n');
-}#include "main.h"
-/**
-* print_alphabet - print alphabet lowercase
-*
-* Return: Always 0.
-*/
-void print_alphabet(void)
-{
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-_putchar('\n');
 }
