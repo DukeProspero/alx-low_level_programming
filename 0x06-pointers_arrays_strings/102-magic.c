b2ce2e5019ea09b84af5741d4df5ec1050f5bf35
DukeProspero <sotinoyeprosper01@gmail.com>
2022-09-22 14:33:47 -0700

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - Executes code
+* main - executes code
 * Return: 0
 */
 int main(void)
@@ -8,16 +8,13 @@ int main(void)
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
 p = &n;
 /*
-* write your line of code here...
 * Remember:
 * - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
-* - you are not allowed to code anything else than this line of cod
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
