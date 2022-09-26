8dab4d81c9a11f144e60009b87abb96331acb716
DukeProspero <sotinoyeprosper01@gmail.com>
2022-09-26 13:48:48 -0700

    3.strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,44 +1,31 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
- **/
+ *_strspn - return the number of occurent of a string
+ * @s: string to check
+ * @accept: character to match
+ * Return: int
+ */
 
 unsigned int _strspn(char *s, char *accept)
 
 {
-int i, j;
-int count = 0
-char *str1, *str2;
+int i, j, cmpt = 0;
 
-str1 = s;
-str2 = accept;
-
-i = 0;
-while (str1[i] != '\0') /*Declaring WHILE *s */
+for (i = 0; s[i] >= '\0'; i++)
 {
-j = 0;
-while (str2[j] != '\0') /*Declaring WHILE *accept*/
+for (j = 0; accept[j] > '\0'; j++)
 {
-if (str2[j] == str1[i]) /*Evaluate condition*/
+if (s[i] == accept[j])
 {
-count++; /*count number*/
+cmpt++;
 break;
 }
-
-j++;    /*add j+1*/
 }
-
-if (s[i] != accept[j]) /*If aren't equals*/
+if (accept[j] == '\0')
 {
 break;
 }
-
-i++; /*add x+1*/
 }
-
-return (count); /*return the value of count*/
+return (cmpt);
 }
