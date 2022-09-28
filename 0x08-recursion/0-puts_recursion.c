0ec7277fd6593c3f310d452a33f2be21bab505c5
DukeProspero <sotinoyeprosper01@gmail.com>
2022-09-28 14:17:13 -0700

    Task0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,15 @@
  * Return: void
  */
 
-void _puts_recursion(char *s) 
+void _puts_recursion(char *s)
 {
-  if (*s == '\0'
-    {
-      _putchar('\n'); 
-    }
-  else
-    {
-      _putchar(*s);
-      _puts_recursion(s + 1);      
-    }
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
 }
