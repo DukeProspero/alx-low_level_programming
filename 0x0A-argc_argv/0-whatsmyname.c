1968306cb145a7775536464aa001e3a3b2d83352
DukeProspero <sotinoyeprosper01@gmail.com>
2022-09-30 14:50:41 -0700

    Task0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+* Return: always zero
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
