e525457f5d123c9364c71cb98a2ce63e75abfd08
DukeProspero <sotinoyeprosper01@gmail.com>
2022-10-03 13:45:10 -0700

    Inserted putchar

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,20 +5,25 @@
  *@c: char
  *Return: pointer
  */
+
 char *create_array(unsigned int size, char c)
 {
 char *buffer;
 unsigned int position;
+
 if (size == 0)
 {
 return (NULL);
 }
+
 /*Define values with malloc*/
 buffer = (char *) malloc(size * sizeof(c));
+
 if (buffer == 0)
 {
 return (NULL);
 }
+
 else
 {
 position = 0;
@@ -27,6 +32,8 @@ while (position < size) /*While for array*/
 *(buffer + position) = c;
 position++;
 {
+
 return (buffer);
 }
+
 }
