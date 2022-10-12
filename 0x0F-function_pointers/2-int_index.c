170219e7e5c095cd5b283c9fee541abc4cd5d1f7
DukeProspero <sotinoyeprosper01@gmail.com>
2022-10-12 14:36:04 -0700

    Task 2: To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,26 +3,19 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @array: input integer array.
- * @size: size of the array.
- * @cmp: pointer to the function to be used
- * to compare values.
- *
- * Return: index of the first eement for which the cmp
- * function does not return 0. If no elements matches,
- * return -1. If size <= 0, return -1.
+ * int_index - indexes aray i think
+ * @array: array to go through
+ * @size: size of array
+ * @cmp: function to do something with size
+ * Return: int value
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-  
-if (array && cmp)
-{
-if (size <= 0)
-return (-1)
-
+if (size <= 0 || array == NULL || cmp == NULL)
+return (-1);
 for (i = 0; i < size; i++)
+{
 if (cmp(array[i]))
 return (i);
 }
